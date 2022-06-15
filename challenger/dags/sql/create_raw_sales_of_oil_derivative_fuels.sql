2109f44681516afd0b4d1b5a5ac7db4502b92480
caiquecos <75267412+caiquecos@users.noreply.github.com>
2022-06-15 11:17:36 -0300

    Delete challenger directory

--- challenger/dags/sql/create_raw_sales_of_oil_derivative_fuels.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS raw_sales_of_oil_derivative_fuels(
-    year_month	date,
-    uf	varchar,
-    product	varchar,
-    unit	varchar,
-    volume	numeric(10,2),
-    created_at	timestamp
-)
